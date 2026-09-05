b30411c21279d5b8d49ff771ed818f1c5eff4844
LeoTellez <leotellez079@gmail.com>
2026-09-04 23:32:45 -0600

    Tipos de datos 04-09-2026

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("Hola mundo")
+
+var num1=23;
+let num2=65;
+const num3=22;
+
+let nombre: string;
+let num: number;
+let activo: boolean;
+let sum: number = num1+num2+num3;
+
+console.log("La suma es: "+sum);
+
+nombre = "Pedro";
+console.log(nombre);
+
+console.log(nombre);
+console.log("Prueba");
