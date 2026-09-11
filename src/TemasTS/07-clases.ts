2f7c9aca3bc442d7e05bf9ff26c537d154866927
LeoTellez <leotellez079@gmail.com>
2026-09-10 19:22:39 -0600

    ts Final 10-09-2026

---
+++ src/TemasTS/07-clases.ts
@@ -0,0 +1,22 @@
+class Persona {
+ 
+    nombre:string;
+    edad:number
+ 
+    constructor(nombre:string, edad:number) {
+        this.nombre=nombre
+        this.edad=edad
+       
+    }
+ 
+ 
+    imprimir(){
+        console.log(`Hola me llamo ${this.nombre} y
+            tengo ${this.edad}  años`)
+    }
+}
+ 
+let persona:Persona
+persona = new Persona('mario',22)
+persona.imprimir()
+ 
